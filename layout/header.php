b603447b5d3b3a4a2bf750283fed382fcff294cc
Diego Freile <dfreile51@gmail.com>
2022-11-10 13:41:11 +0100

    Boton de inicio de sesion cambiado

--- layout/header.php
+++ layout/header.php
@@ -1,7 +1,7 @@
 <!-- Inicio del encabezado -->
 <header class="py-3 bg-light">
     <div class="container-fluid d-flex flex-wrap justify-content-center">
-        <a href="#" class="d-flex align-items-center mb-lg-0 text-black text-decoration-none">
+        <a href="../index.php" class="d-flex align-items-center mb-lg-0 text-black text-decoration-none">
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" fill="currentColor" class="bi bi-vinyl-fill me-2" viewBox="0 0 16 16">
                 <path d="M8 6a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm0 3a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4 8a4 4 0 1 0 8 0 4 4 0 0 0-8 0z"/>
@@ -40,9 +40,10 @@
             </ul>
 
             <!-- Boton de inicio de sesion -->
-            <form class="d-flex">
-                <button class="btn btn-outline-warning d-md-inline-block m-auto" type="submit">Iniciar Sesión</button>
-            </form>
+            <div class="d-flex">
+                <a class="d-md-inline-block btn btn-outline-warning m-auto" tabindex="-1" role="button" href="php/iniciar_sesion.php">Iniciar Sesión</a>
+            </div>
+           
         </div>
     </div>
 </nav>
